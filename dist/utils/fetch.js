ae25e0114566d81081b7b14105aaca5d89cbe325
Casper Verswijvelt <casperverswijvelt@gmail.com>
2024-10-11 23:55:48 +0200

    chore: update build

--- dist/utils/fetch.js
+++ dist/utils/fetch.js
@@ -4,6 +4,7 @@ exports.fetchOverride = void 0;
 function fetchOverride(input, init, userAgent) {
     var _a;
     if (userAgent === void 0) { userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"; }
+    console.log({ userAgent: userAgent });
     var newInit = init !== null && init !== void 0 ? init : {};
     newInit.headers = (_a = newInit.headers) !== null && _a !== void 0 ? _a : {};
     newInit.headers["User-Agent"] = userAgent;
--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -3,6 +3,7 @@ export function fetchOverride(
   init?: RequestInit,
   userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
 ): Promise<Response> {
+  console.log({ userAgent });
   const newInit = init ?? {};
   newInit.headers = newInit.headers ?? {};
   (newInit.headers as any)["User-Agent"] = userAgent;
